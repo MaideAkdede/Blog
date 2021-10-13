993b4186a4439ee1b03ca697d95e73bcf7d14266
MaideAkdede <maide.akdede@student.hepl.com>
2021-10-13 13:44:43 +0200

    :pencil2: change echo view

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -6,7 +6,7 @@
         {{ $trigger }}
     </div>
     {{-- Entries - Items - Links --}}
-    <div x-show="show" class="py-2 absolute bg-gray-100 rounded-xl w-full z-50 mt-2" style="display: none">
+    <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50 overflow-auto max-h-52" style="display: none">
         {{ $entries }}
     </div>
 </div>
