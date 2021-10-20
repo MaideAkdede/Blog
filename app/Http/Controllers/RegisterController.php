6e5e42a007ba911561ddb2524fc931b9dc731b86
Maïde <maide.akdede@hotmail.com>
2021-10-20 13:55:11 +0200

    :sparkles: Form Request Validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Validation\Rule;
 use PhpParser\Node\Stmt\Return_;
@@ -12,20 +14,9 @@ class RegisterController extends Controller
     {
         return view('register.create');
     }
-
-    public function store()
+    public function store(StoreUserRequest $r)
     {
-        //Valider nos données validate fournis par l’objet request
-        // arg. de validation
-        $validateData = request()->validate([
-            'name' => ['required','min:3', 'max:255'],
-            'username' => ['required', 'min:3', 'max:255', Rule::unique('users', 'username')],
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:6|max:48',
-        ]);
-
-        User::create($validateData);
-
-        return 'User Crée';
+        User::create($r->validated());
+        return redirect('/')->with('success', __('messages.account-created'));
     }
 }
