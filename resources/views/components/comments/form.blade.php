8e712ec7d9a3151bfad8138f5085634cefab2feb
Maïde <maide.akdede@hotmail.com>
2021-10-25 14:36:35 +0200

    :pencil2: externalize Button and add Error Msg

--- resources/views/components/comments/form.blade.php
+++ resources/views/components/comments/form.blade.php
@@ -2,10 +2,11 @@
 
 <h2 class="mb-4 text-3xl font-bold text-center">Want to participate ?</h2>
 @guest()
-    <p class="text-center"><a href="/resgister" class="font-bold underline hover:text-blue-500">Register</a> or <a href="/login" class="font-bold underline hover:text-blue-500">Login</a> to add a comment</p>
+    <p class="text-center"><a href="/resgister" class="font-bold underline hover:text-blue-500">Register</a> or <a
+            href="/login" class="font-bold underline hover:text-blue-500">Login</a> to add a comment</p>
 @else
     <form action="/posts/{{ $post->id }}/comments" method="POST" class="mt-10"
-    class="">
+          class="">
         @csrf
         <header class="flex items-center mb-4">
             <img src="https://i.pravatar.cc/60?u={{auth()->id()}}"
@@ -14,9 +15,11 @@
                  class="mr-4 rounded-full">
             <label for="body" class="font-bold">Add a comment</label>
         </header>
-        <textarea placeholder="What are your thoughts, tell us about it!" class="p-2 w-full border border-gray-400" name="body" id="body" cols="30" rows="10"></textarea>
+        <textarea placeholder="What are your thoughts, tell us about it!" class="p-2 w-full border border-gray-400"
+                  name="body" id="body" cols="30" rows="10"></textarea>
+        <x-error-message field="body"/>
         <div class="flex justify-end">
-            <button class="py-2 px-4 mt-4 font-bold text-white uppercase bg-blue-400 rounded hover:bg-blue-500" type="submit">POST</button>
+           <x-submit-button>Post my comment</x-submit-button>
         </div>
     </form>
 @endguest
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,12 +1,17 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-</head>
-<title>Laravel From Scratch Blog</title>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-<script src="//unpkg.com/alpinejs" defer></script>
 <head>
+    <title>Laravel From Scratch Blog</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <script src="//unpkg.com/alpinejs" defer></script>
+    <style>
+        html{
+            scroll-behavior: smooth;
+        }
+    </style>
+</head>
 <body style="font-family: Open Sans, sans-serif">
 <section class="px-6 py-8">
     <nav class="md:flex md:justify-between md:items-center">
@@ -27,7 +32,7 @@
                     <button type="submit" class="text-xs uppercase semi-bold text-blue-500 m-6">Logout</button>
                 </form>
             @endguest
-            <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+            <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                 Subscribe for Updates
             </a>
         </div>
@@ -35,7 +40,7 @@
 
     {{ $mainContent }}
 
-    <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+    <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
         <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
         <h5 class="text-3xl">Stay in touch with the latest posts</h5>
         <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
