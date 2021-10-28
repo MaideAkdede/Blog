4dcb68e6f2c52a6764f5f3483fca61f5dc5b9675
Maïde <maide.akdede@hotmail.com>
2021-10-28 14:23:57 +0200

    :memo: Newsletter

--- app/Services/ConvertKitNewsletter.php
+++ app/Services/ConvertKitNewsletter.php
@@ -4,7 +4,7 @@
 namespace App\Services;
 
 
-class CovertKitNewsletter implements Newsletter
+class ConvertKitNewsletter implements Newsletter
 {
     public function subscribe(string $email, string $list = null)
     {
